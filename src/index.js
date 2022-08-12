ab6c78ce43477ccbf53930674448f169c2fc9117
Sofia Isabelle <sofia.i.rahimi@gmail.com>
2022-08-12 11:14:31 -0400

    copy project over

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/* 
+1) Import the react and ReactDOM libraries
+2) Get a reference to the div ID with ID root 
+3) Tell React to take control of that element
+4) Create Component 
+5) Show the Component on the screen   
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
